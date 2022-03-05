3467e8cc71da314e3a94c6f2a9d702245acda795
lvchengchang <11647791@qq.com>
2022-03-05 12:55:00 +0800

    1) reset

--- main.go
+++ main.go
@@ -22,9 +22,17 @@ const (
 )
 
 var uploadTemplate = template.Must(template.ParseFiles("encrypt.html"))
+var deUploadTemplate = template.Must(template.ParseFiles("decrypt.html"))
 
 func indexHandle(w http.ResponseWriter, r *http.Request) {
 	if err := uploadTemplate.Execute(w, nil); err != nil {
+		log.Fatal("Execute: ", err.Error())
+		return
+	}
+}
+
+func decrypt(w http.ResponseWriter, r *http.Request) {
+	if err := deUploadTemplate.Execute(w, nil); err != nil {
 		log.Fatal("Execute: ", err.Error())
 		return
 	}
@@ -60,7 +68,7 @@ func uploadHandle(w http.ResponseWriter, r *http.Request) {
 	if err != nil {
 		fmt.Println("err")
 	}
-	file, err = os.Open("./uploads/" + handler.Filename + "dst")
+	file, err = os.Open("./uploads/" + handler.Filename + ".dst")
 	if err != nil {
 		fmt.Println(err)
 		return
@@ -78,8 +86,58 @@ func uploadHandle(w http.ResponseWriter, r *http.Request) {
 	}
 }
 
+func deUploadHandle(w http.ResponseWriter, r *http.Request) {
+	r.ParseMultipartForm(32 << 20)
+	file, handler, err := r.FormFile("file")
+	if err != nil {
+		fmt.Println(err)
+		return
+	}
+	defer file.Close()
+	f, err := os.OpenFile("./de_uploads/"+handler.Filename, os.O_WRONLY|os.O_CREATE, 0666) // 此处假设当前目录下已存在test目录
+	if err != nil {
+		return
+	}
+	defer f.Close()
+	io.Copy(f, file)
+
+	mod := flag.String("mod", "md5", "md5,sha1,sha256,crc32")
+	h, ok := map[string]hash.Hash{
+		"md5":    md5.New(),
+		"sha1":   sha1.New(),
+		"sha256": sha256.New(),
+		"crc32":  crc32.NewIEEE(),
+	}[*mod]
+	if !ok {
+		h = md5.New()
+	}
+	// 进行文件
+	err = decFile("./de_uploads/"+handler.Filename, h)
+	if err != nil {
+		fmt.Println("err")
+	}
+	file, err = os.Open("./de_uploads/" + handler.Filename + ".src")
+	if err != nil {
+		fmt.Println(err)
+		return
+	}
+	defer file.Close()
+	content, err := ioutil.ReadAll(file)
+	fileNames := url.QueryEscape("./de_uploads/" + handler.Filename + ".src") // 防止中文乱码
+	w.Header().Add("Content-Type", "application/octet-stream")
+	w.Header().Add("Content-Disposition", "attachment; filename=\""+fileNames+"\"")
+
+	if err != nil {
+		fmt.Println("Read File Err:", err.Error())
+	} else {
+		w.Write(content)
+	}
+}
+
 func main() {
 	http.HandleFunc("/", indexHandle)
+	http.HandleFunc("/decrypt", decrypt)
 	http.HandleFunc("/uploads", uploadHandle)
+	http.HandleFunc("/de_uploads", deUploadHandle)
 	http.ListenAndServe(":8080", nil)
 }
